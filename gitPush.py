d16225160956ae6338992638ff50959c775b923a
hookbye <weljun@sina.com>
2018-08-03 18:07:56 +0800

    quick commit for save

--- gitPush.py
+++ gitPush.py
@@ -9,12 +9,11 @@ def gitCmd(cmd):
 def gitPush():
 	gitCmd("git add -A")
 	commitInfo = raw_input()
-	print type(commitInfo)
 	print commitInfo == ""
 	if commitInfo == "":
 		commitInfo = "\"quick commit for save\""
 	gitCmd("git ci -m "+commitInfo)
 	gitCmd("git pl")
-	# gitCmd("git ps")
+	gitCmd("git ps")
 if __name__ == '__main__':
 	gitPush()
